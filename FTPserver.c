4c2e63c3aaf7cea84abf5890420277cc1ac00ab2
Adri <ahmadadri001@gmail.com>
2022-05-29 13:32:21 +0800

    Server (edited)

--- FTPserver.c
+++ FTPserver.c
@@ -6,18 +6,20 @@
 #include <unistd.h>
 #define SIZE 1024
 
+//create a new file and write to it
 void write_file(int socket_desc){
 	int i;
 	FILE *fp;
-	char *filename = "adri2.txt";
+	char *filename = "adri2.txt";//file to be created
 	char buffer[SIZE];
 
 	fp = fopen(filename, "w");
         if(fp == NULL){
-                perror("Error in creating file.");
+                perror("[!]Error in creating file.");
                 exit(1);
         }
 
+	//Write to the file
 	while(1){
 		i = recv(socket_desc, buffer, SIZE, 0);
 		if(i <= 0){
@@ -27,6 +29,7 @@ void write_file(int socket_desc){
 		fprintf(fp, "%s", buffer);
 		bzero(buffer, SIZE);
 	}
+	printf("[+]File successfully received\n");
 
 	return;
 }
@@ -39,7 +42,7 @@ int main(int argc, char *argv[]){
 	//Create socket
 	socket_desc = socket(AF_INET, SOCK_STREAM, 0);
 	if(socket_desc < 0){
-		printf("Could not create socket");
+		printf("[!]Could not create socket");
 		return 1;
 	}
 
@@ -50,25 +53,26 @@ int main(int argc, char *argv[]){
 
 	//Bind
 	if(bind(socket_desc, (struct sockaddr *)&server, sizeof(server)) < 0){
-		puts("Bind failed");
+		puts("[!]Bind failed");
 		return 1;
 	}
-	puts("Bind done");
 
 	//Listen
 	listen(socket_desc, 3);
 
-	puts("Waiting for incoming connections...");
+	puts("[+]Listening on port 8888...");
 	c = sizeof(struct sockaddr_in);
 	new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
 	if(new_socket < 0){
-                perror("accept failed");
+                perror("[!]Connection failed");
                 return 1;
         }
-	puts("Connection accepted");
+	puts("[+]Connection accepted");
 
 	write_file(new_socket);
-	printf("Data has been written in the text file.\n");
+
+	close(socket_desc);
+	puts("[+]Connection closed");
 
 	return 0;
 }
